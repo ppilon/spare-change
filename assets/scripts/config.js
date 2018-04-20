69d0cd7507053f458f1cec8b3d53a64a5d57f78c
ppilon <pilon24@icloud.com>
2018-04-19 23:51:29 -0400

    Added heroku production link to config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://limitless-reaches-50738.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
